2fe8c36e0026df25b3176d49a2dcd86ea89dc7b4
James Sawyer <sawyer12@gmail.com>
2020-10-21 18:20:09 +0100

    update disclaimer 2020

--- Python_script_example_PyTrader.py
+++ Python_script_example_PyTrader.py
@@ -1,3 +1,21 @@
+'''THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+NON-INFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR ANYONE
+DISTRIBUTING THE SOFTWARE BE LIABLE FOR ANY DAMAGES OR OTHER LIABILITY,
+WHETHER IN CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
+
+# Bitcoin Cash (BCH)   qpz32c4lg7x7lnk9jg6qg7s4uavdce89myax5v5nuk
+# Ether (ETH) -        0x843d3DEC2A4705BD4f45F674F641cE2D0022c9FB
+# Litecoin (LTC) -     Lfk5y4F7KZa9oRxpazETwjQnHszEPvqPvu
+# Bitcoin (BTC) -      34L8qWiQyKr8k4TnHDacfjbaSqQASbBtTd
+
+# contact :- github@jamessawyer.co.uk
+
+
+
 """
     PyTrader api - script example for easy connection with Metatrader4 EA & Metatrader5 EA
 	This is an example file with all calling functions to metatrader terminal and get it back to your script
